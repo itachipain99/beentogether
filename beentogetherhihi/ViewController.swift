672e6e85d9a118e5bae496205151c065638b2247
hieu <itachipain11@gmail.com>
2019-10-03 00:14:32 +0700

    Initial Commit

---
+++ beentogetherhihi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beentogetherhihi
+//
+//  Created by Nguyễn Minh Hiếu on 10/3/19.
+//  Copyright © 2019 Nguyễn Minh Hiếu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
